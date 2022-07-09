796a2149d2ff9beb45364a073f198b51bfa96a08
bvramana1312 <bvramanamscis@gmail.com>
2022-07-09 11:35:58 +0530

    initial changes

---
+++ src/main/java/com/SampleSB01/SampleSb01Application.java
@@ -0,0 +1,13 @@
+package com.SampleSB01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SampleSB01/SampleSb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SampleSB01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
